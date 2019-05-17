324f055561e3a95e7a187eff5c771e1a97621827
matumoro <49759583+matumoro@users.noreply.github.com>
2019-05-17 09:49:34 -0500

    Update Controllers y Views

--- Sales.Common/Models/Product.cs
+++ Sales.Common/Models/Product.cs
@@ -2,11 +2,13 @@
 {
     using System;
     using System.ComponentModel.DataAnnotations;
+
     public class Product
     {
         [Key]
         public int ProductId{ get; set; }
-        public int Description{ get; set; }
+        [Required]
+        public string Description{ get; set; }
         public Decimal Price{ get; set; }
         public bool IsAvailable { get; set; }
         public DateTime PublishOn { get; set; }
--- Sales.Domain/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Sales.Domain
-{
-    public class Class1
-    {
-    }
-}
