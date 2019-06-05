83a49490ac5523147f9494fd06bc981b87574052
matumoro <49759583+matumoro@users.noreply.github.com>
2019-06-05 10:22:44 -0500

    UPDATE

---
+++ Sales/Sales/Services/ApiService.cs
@@ -0,0 +1,21 @@
+﻿namespace Sales.Services
+{
+    using System.Threading.Tasks;
+    
+    public class ApiService
+    {
+        public async Task<Response> GetList<T>(string urlBase, string prefix, string controller)
+        {
+            try
+            {
+
+            }
+            catch (System.Exception)
+            {
+
+                throw;
+            }
+        }
+
+    }
+}
