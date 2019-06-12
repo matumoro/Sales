ffc984b6d0c1a9b80493d496570164e706cafb14
matumoro <49759583+matumoro@users.noreply.github.com>
2019-06-12 13:39:08 -0500

    Update Video 12

--- Sales.Common/Models/Product.cs
+++ Sales.Common/Models/Product.cs
@@ -7,10 +7,26 @@
     {
         [Key]
         public int ProductId{ get; set; }
+
         [Required]
-        public string Description{ get; set; }
+        [StringLength(50)]
+         public string Description{ get; set; }
+
+        [DataType (DataType.MultilineText)]
+        public string Remarks { get; set; }
+
+
+        [Display(Name ="Image")]
+        public string ImagePath { get; set; }
+
+        [DisplayFormat(DataFormatString ="{0:C}",ApplyFormatInEditMode =false)]
         public Decimal Price{ get; set; }
+
+        [Display(Name = "Is Available")]
         public bool IsAvailable { get; set; }
+
+        [Display(Name = "Publish On")]
+        [DataType(DataType.Date)]
         public DateTime PublishOn { get; set; }
        
         public override string ToString()
