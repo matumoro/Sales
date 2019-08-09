279064e709a87d6b010101d5e56515bbe31167c7
matumoro <49759583+matumoro@users.noreply.github.com>
2019-08-09 16:21:56 -0500

    update video 16-17

--- Sales.Common/Models/Product.cs
+++ Sales.Common/Models/Product.cs
@@ -35,7 +35,7 @@
            {
                 if (string.IsNullOrEmpty(this.ImagePath))
                 {
-                    return null;
+                    return "noproduct";
 
                 }
                 return $"https://salesbackend20190608022117.azurewebsites.net/{this.ImagePath.Substring(1)}";
