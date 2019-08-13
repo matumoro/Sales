bbdce930d7b7a14581d2072851bed0e7d194de13
matumoro <49759583+matumoro@users.noreply.github.com>
2019-08-13 11:58:54 -0500

    Update Video 26

--- Sales/Sales.Android/MainActivity.cs
+++ Sales/Sales.Android/MainActivity.cs
@@ -1,15 +1,17 @@
-﻿using System;
-
-using Android.App;
-using Android.Content.PM;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
-using Android.OS;
-
-namespace Sales.Droid
+﻿namespace Sales.Droid
 {
-    [Activity(Label = "Sales", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    using Android.App;
+    using Android.Content.PM;
+    using Android.Runtime;
+    using Android.OS;
+    using Plugin.Permissions;
+    using Plugin.CurrentActivity;
+
+    [Activity(Label = "Sales", 
+        Icon = "@mipmap/icon", 
+        Theme = "@style/MainTheme", 
+        MainLauncher = true, 
+        ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
@@ -18,16 +20,25 @@ namespace Sales.Droid
             ToolbarResource = Resource.Layout.Toolbar;
 
             base.OnCreate(savedInstanceState);
-
+            CrossCurrentActivity.Current.Init(this, savedInstanceState);
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             LoadApplication(new App());
         }
-        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        public override void OnRequestPermissionsResult(
+            int requestCode, 
+            string[] permissions, 
+            [GeneratedEnum] Permission[] grantResults)
         {
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
 
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            PermissionsImplementation.Current.OnRequestPermissionsResult(
+            requestCode,
+            permissions,
+            grantResults);
         }
+               
     }
 }
--- Sales/Sales/ViewModels/AddProductViewModel.cs
+++ Sales/Sales/ViewModels/AddProductViewModel.cs
@@ -1,9 +1,12 @@
 ﻿namespace Sales.ViewModels
 {
+    using System;
     using System.Linq;
     using System.Windows.Input;
     using GalaSoft.MvvmLight.Command;
     using Helpers;
+    using Plugin.Media;
+    using Plugin.Media.Abstractions;
     using Sales.Common.Models;
     using Services;
     using Xamarin.Forms;
@@ -11,10 +14,12 @@
     public class AddProductViewModel:BaseViewModel
     {
         #region Services
-            private ApiService apiService;
+            private readonly ApiService apiService;
         #endregion
 
         #region Attributes
+            private MediaFile file;
+            private ImageSource imageSource;
             private bool isRunning;
             private bool isEnabled;
         #endregion
@@ -34,6 +39,11 @@
                 get { return this.isEnabled; }
                 set { SetValue(ref this.isEnabled, value); }
             }
+            public ImageSource ImageSource
+            {
+                get { return this.imageSource; }
+                set { SetValue(ref this.imageSource, value); }
+            }
         #endregion
 
         #region Constructors
@@ -41,10 +51,65 @@
         {
             this.apiService = new ApiService();
             this.IsEnabled = true;
+            this.ImageSource = "noproduct";
         }
         #endregion
 
         #region Commands
+
+            public ICommand ChangeImageCommand
+            {
+                get
+                {
+                return new RelayCommand(ChangeImage);
+                }
+                
+            }
+
+            private async void ChangeImage()
+            {
+                await CrossMedia.Current.Initialize();
+
+                var source = await Application.Current.MainPage.DisplayActionSheet(
+                    Languages.ImageSource,
+                    Languages.Cancel,
+                    null,
+                    Languages.FromGallery,
+                    Languages.NewPicture);
+
+                if (source==Languages.Cancel)
+                {
+                    this.file = null;
+                    return;
+                }
+
+                if (source == Languages.NewPicture)
+                {
+                    this.file = await CrossMedia.Current.TakePhotoAsync(new StoreCameraMediaOptions
+                    {
+                        Directory = "Sample",
+                        Name = "test.jpg",
+                        PhotoSize = PhotoSize.Small,
+                    }
+                    );
+                }
+                else
+                {
+                    this.file = await CrossMedia.Current.PickPhotoAsync();
+                }
+
+                if (this.file != null)
+                {
+                    this.ImageSource = ImageSource.FromStream(() =>
+                    {
+                        var stream = this.file.GetStream();
+                        //this.file.Dispose();
+                        return stream;
+                    });
+                }
+
+            }
+
             public ICommand SaveCommand
             {
                 get
@@ -85,13 +150,13 @@
                 return;
                 }
 
-                this.isRunning = true;
+                this.IsRunning = true;
                 this.IsEnabled = false;
 
                 var connection = await this.apiService.CheckConnection();
                 if (!connection.IsSuccess)
                 {
-                    this.isRunning = false;
+                    this.IsRunning = false;
                     this.IsEnabled = true;
                     await Application.Current.MainPage.DisplayAlert(
                     Languages.Error, 
@@ -117,7 +182,7 @@
 
                 if (!response.IsSuccess)
                 {
-                    this.isRunning = false;
+                    this.IsRunning = false;
                     this.IsEnabled = true;
                     await Application.Current.MainPage.DisplayAlert(
                     Languages.Error,
@@ -130,7 +195,7 @@
                 var viewModel = ProductsViewModel.GetInstance();
                 viewModel.Products.Add(newProduct);
 
-                this.isRunning = false;
+                this.IsRunning = false;
                 this.IsEnabled = true;
 
                 await Application.Current.MainPage.Navigation.PopAsync();
